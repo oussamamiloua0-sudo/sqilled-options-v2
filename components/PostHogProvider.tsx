93cc4133fe08ab0403f18bc45fa5746cf7f8202b
bkkozy <oussamamiloua0@gmail.com>
2026-03-21 17:40:12 +0000

    PostHog: use instrumentation-client.ts for Next.js 15.3+ as per docs

--- components/PostHogProvider.tsx
+++ components/PostHogProvider.tsx
@@ -3,17 +3,6 @@
 import posthog from 'posthog-js';
 import { PostHogProvider as PHProvider } from 'posthog-js/react';
 
-if (typeof window !== 'undefined') {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST ?? 'https://us.i.posthog.com',
-    capture_pageview: true,
-    capture_pageleave: true,
-    loaded: (ph) => {
-      if (process.env.NODE_ENV === 'development') ph.debug();
-    },
-  });
-}
-
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   return <PHProvider client={posthog}>{children}</PHProvider>;
 }
