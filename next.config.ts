1e1d71e32ded9096c241039d9e6ea635db70a247
oussamamiloua0-sudo <oussamamiloua0@gmail.com>
2026-04-22 19:51:09 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  skipTrailingSlashRedirect: true,
+  async rewrites() {
+    return [
+      { source: '/ingest/static/:path*', destination: 'https://us-assets.i.posthog.com/static/:path*' },
+      { source: '/ingest/:path*',        destination: 'https://us.i.posthog.com/:path*' },
+    ];
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
